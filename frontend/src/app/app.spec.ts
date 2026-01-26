0e69caf0df06bc1fda023d0a189f494009d1af35
rockandlog <aleksandra.jaskolska@gamil.com>
2026-01-26 23:18:26 +0100

    Zaliczone testy frontendu i poprawiona konfiguracja Dockera

--- frontend/src/app/app.spec.ts
+++ frontend/src/app/app.spec.ts
@@ -1,34 +1,37 @@
 import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app';
-import { provideHttpClient } from '@angular/common/http';
-import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { AppComponent } from './app'; 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      // Importujemy Twój komponent
-      imports: [AppComponent],
-      // Dostarczamy "udawany" internet, żeby testy nie próbowały naprawdę łączyć się z backendem
-      providers: [
-        provideHttpClient(),
-        provideHttpClientTesting()
-      ]
+      imports: [AppComponent, HttpClientTestingModule, FormsModule],
     }).compileComponents();
   });
 
-  // Test 1: Sprawdza, czy aplikacja w ogóle się tworzy (czy nie ma błędów w kodzie)
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  // Test 2: Sprawdza, czy tytuł w kodzie HTML jest poprawny
-  it('should render title', () => {
+  it(`should have default currency as 'USD'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    // Sprawdzamy czy w nagłówku h1 jest tekst "Projekt Waluty"
-    expect(compiled.querySelector('h1')?.textContent).toContain('Projekt Waluty');
+    const app = fixture.componentInstance;
+    expect(app.wybranaWaluta).toEqual('USD');
+  });
+
+  it('should have default year as 2025', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.wybranyRok).toEqual(2025);
+  });
+
+  it('should start with isLoading = false', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    
+    expect(app.isLoading).toBe(false);
   });
 });
